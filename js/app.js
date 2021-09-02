f276f82ea83616035ab7b806a7d520b9a5b9c677
istiaque ahemed <istiaquepersonal@gmail.com>
2021-09-02 12:42:05 +0600

    book data show

--- js/app.js
+++ js/app.js
@@ -2,26 +2,39 @@ const getData = () => {
     const inputField = document.getElementById('input-field')
     const inputValue = inputField.value;
 
-
     fetch(`http://openlibrary.org/search.json?q=${inputValue}`)
         .then(res => res.json())
         .then(data => displayData(data.docs))
+    const errorDiv = document.getElementById('error')
+    if (inputValue === '') {
+        errorDiv.innerText = "Search field cannot be empty"
+    }
+
 }
 getData()
 
 const displayData = (data) => {
+
+
+
     const containerDiv = document.getElementById('search-result');
-    // const allData = data.docs;
+
+
+    // clear dom 
+    containerDiv.innerHTML = '';
+
     data.forEach(item => {
+
         const div = document.createElement('div');
         div.classList.add('card-sytyle')
         div.classList.add('col');
         div.innerHTML = `
-        <div  class="card h-100">
-        <img src="" class="card-img-top" alt="...">
+        <div  class="card h-40">
+        <img id="img" class="card-img-top" src="https://covers.openlibrary.org/b/id/${item.cover_i}-M.jpg" />
         <div class="card-body">
-            <h5 class="card-title">${item.title}</h5>
-            <p class="card-text"> ${item.author_name}</p>
+            <h5 class="card-text"><b id="text-success">Book Name: </b>${item.title.slice(0,)}</h5>
+            <p class="card-text"><b id="text-success">Book Author: </b> ${item.author_name[0]}</p>
+            <p class="card-text"><b id="text-success">Book 1st publish year: </b> ${item.first_publish_year}</p>
         </div>
     </div>
 </div>
@@ -32,5 +45,4 @@ const displayData = (data) => {
 
 
 }
-// https://covers.openlibrary.org/b/id/{cover_i}-M.jpg
 
