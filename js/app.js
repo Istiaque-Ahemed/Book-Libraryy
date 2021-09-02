7e04e2f6a58bae2aef0479fdcfbe44c1847c5a3b
istiaque ahemed <istiaquepersonal@gmail.com>
2021-09-02 20:16:10 +0600

    done

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@ const getData = () => {
 
     const inputField = document.getElementById('input-field')
     const inputValue = inputField.value;
-    inputField.value ="";
+    inputField.value = "";
     // ******convate to data json fille
     fetch(`https://openlibrary.org/search.json?q=${inputValue}`)
         .then(res => res.json())
@@ -20,25 +20,25 @@ const displayData = (data) => {
     let inputFields = document.getElementById('input-field')
     let inputValues = inputFields.value;
     let errorDiv = document.getElementById('error')
-    
+
 
 
 
 
 
     let count = Object.keys(data).length
-    
+
     //******* Error handle 
     if (inputValues === 'null') {
         errorDiv.innerText = "Search field cannot be empty"
-    } 
-     if (count > 0) {
+    }
+    if (count > 0) {
         errorDiv.innerText = count + " Items found!"
     } else {
         errorDiv.innerText = "No item founds!"
     }
-    
-    
+
+
 
 
 
@@ -46,26 +46,27 @@ const displayData = (data) => {
     containerDiv.innerHTML = '';
 
     // Show Data in ui 
-    
+
     data.forEach(item => {
         this.count++;
         const div = document.createElement('div');
         div.classList.add('card-sytyle')
         div.classList.add('col');
         div.innerHTML = `
-        <div  class="card h-40">
-        <img id="img" class="card-img-top" src="https://covers.openlibrary.org/b/id/${item.cover_i}-M.jpg" />
-        <div class="card-body">
-            <h5 class="card-text"><b id="text-success">Book Name: </b>${item.title}</h5>
-            <p class="card-text"><b id="text-success">Book Author: </b> ${item.author_name}</p>
-            <p class="card-text"><b id="text-success">Book 1st publish year: </b> ${item.first_publish_year}</p>
-        </div>
-    </div>
-</div>
-    `
+        <div class="card">
+      <img src="https://covers.openlibrary.org/b/id/${item.cover_i}-M.jpg"
+      class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-text"><b id="text-success">Book Name: </b>${item.title}</h5>
+        <p class="card-text"><b id="text-success">Book Author: </b> ${item.author_name}</p>
+        <p class="card-text"><b id="text-success">Book 1st publish year: </b> ${item.first_publish_year}</p>
+        
+      </div>
+        `
+
         containerDiv.appendChild(div)
 
-        
+
 
     });
 
